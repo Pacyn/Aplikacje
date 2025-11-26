d5018de321faf2165ac7c4fe2c2d31a1ea47dae3
Pacyn <erykpacyna210@gmail.com>
2025-11-26 22:02:31 +0100

    zadanie4
    yup

---
+++ zadanie4/script.js
@@ -0,0 +1,50 @@
+var xhttp = new XMLHttpRequest();
+var response, city;
+const container = document.getElementById("weatherContainer");
+
+xhttp.onload = function(){    
+    response = JSON.parse(xhttp.responseText);
+    console.log(response);
+    p = createDiv(new Date(response.dt * 1000),
+        response.name, 
+        response.main.temp, 
+        response.main.feels_like,
+        response.weather[0].description);
+    container.appendChild(p);
+    getApiForecast(`https://api.openweathermap.org/data/2.5/forecast?q=${response.name}&appid=2959962c41b9fdf356cef6fb37d0be1b&lang=pl&units=metric`);
+}
+
+function getApi(city){
+    container.textContent = "";
+    city = document.getElementById("textCity").value;
+    if(city == "") return;
+    xhttp.open("GET",`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2959962c41b9fdf356cef6fb37d0be1b&lang=pl&units=metric`);
+    xhttp.send();
+}
+
+async function getApiForecast(api) {
+    tab = [7,15,23,31,39];
+    let x = await fetch(api);
+    let apiResponse = JSON.parse(await x.text());
+    console.log(apiResponse);
+    tab.forEach(element => {
+       p = createDiv(apiResponse.list[element].dt_txt,
+        apiResponse.city.name, 
+        apiResponse.list[element].main.temp,
+        apiResponse.list[element].main.feels_like,
+        apiResponse.list[element].weather[0].description);
+    container.appendChild(p); 
+    });
+}
+
+function createDiv(date, city, temp, feelsLike, weatherCondition){
+    tab = [`Data: ${date}`, `Miejsce: ${city}`,`Temperatura: ${temp}`, `Odczuwalna: ${feelsLike}`, `Kondycja pogody: ${weatherCondition}`];
+    var div = document.createElement("div");
+    div.className = "weather";
+    tab.forEach(element => {
+       var p = document.createElement("p");
+        p.innerText = element;
+        div.appendChild(p); 
+    });
+    return div;
+}
